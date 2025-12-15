ffa981030acfd69311278f59f17dc2f5ee904d6c
santos1513lopes-lgtm <santos1513.lopes@gmail.com>
2025-12-14 21:32:53 -0300

    Adicionando lupa e agrupamento

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 import firebase_admin
 from firebase_admin import credentials, storage, firestore
 import datetime
+from collections import defaultdict
 
 app = Flask(__name__)
 
@@ -20,43 +21,42 @@ db = firestore.client()
 
 @app.route('/')
 def index():
-    # Busca todas as músicas e ordena pela playlist para ficar organizado
-    docs = db.collection('musicas').order_by('playlist').stream()
+    docs = db.collection('musicas').stream()
     
-    lista_musicas = []
+    # Agrupa as músicas por playlist
+    playlists_agrupadas = defaultdict(list)
+    
     for doc in docs:
         dados = doc.to_dict()
         nome_arquivo = dados['nome']
-        # Pega o ID do documento para podermos editá-lo depois
         doc_id = doc.id 
-        playlist_nome = dados.get('playlist', 'Geral')
+        # Pega a playlist e coloca em MAIÚSCULO
+        playlist_nome = dados.get('playlist', 'Geral').upper()
         
         blob = bucket.blob(nome_arquivo)
         url_assinada = blob.generate_signed_url(expiration=datetime.timedelta(hours=1))
         
-        lista_musicas.append({
+        musica_obj = {
             'id': doc_id,
             'nome': nome_arquivo,
             'playlist': playlist_nome,
             'url': url_assinada
-        })
+        }
+        
+        playlists_agrupadas[playlist_nome].append(musica_obj)
 
-    return render_template('index.html', musicas=lista_musicas)
+    return render_template('index.html', playlists=dict(playlists_agrupadas))
 
-# --- NOVA FUNÇÃO: ATUALIZAR PLAYLIST ---
 @app.route('/atualizar', methods=['POST'])
 def atualizar_playlist():
-    # Recebe os dados do formulário do site
     id_musica = request.form['id_musica']
     nova_playlist = request.form['nova_playlist']
     
-    # Atualiza no Banco de Dados
     doc_ref = db.collection('musicas').document(id_musica)
     doc_ref.update({
         'playlist': nova_playlist
     })
     
-    # Recarrega a página para mostrar a mudança
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
